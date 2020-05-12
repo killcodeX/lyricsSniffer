62ebc5519a6570870a5a01f837435f1589e3e4a0
Aaquib Ahmed <aaquibskimbox@gmail.com>
2020-05-12 22:36:52 +0530

    added react dom

--- lyrics-sniffer/src/App.js
+++ lyrics-sniffer/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
 import Nav from './components/layouts/nav';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Nav/>
-    </div>
+    <Router>
+      <React.Fragment>
+        <Nav />
+      </React.Fragment>
+    </Router>
   );
 }
 
