d92da6e455f2b2c737203d77cb50c659a2743e42
Aaquib Ahmed <aaquibskimbox@gmail.com>
2020-05-13 22:45:24 +0530

    added spinner to show loading

--- lyrics-sniffer/src/components/tracks/tracks.js
+++ lyrics-sniffer/src/components/tracks/tracks.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import { Consumer } from '../../context';
-
+import Spinner from '../layouts/spinner';
 class Tracks extends Component {
     render() {
         return (
             <div>
                <Consumer>
                    {value=>{
-                       console.log(value);
-                       return <h1>Tracks</h1>
+                       const { track_list } = value;
+                       if( track_list === undefined || track_list.length == 0){
+                           return <Spinner />
+                       }
+                       else{
+                        return <h1>Tracks Loaded...</h1>
+                       }
                    }}
                </Consumer>
             </div>
