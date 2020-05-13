a3353827510d1c4a7608e15ce1eb0eb069beb68c
Aaquib Ahmed <aaquibskimbox@gmail.com>
2020-05-14 02:15:55 +0530

    set the track cards

--- lyrics-sniffer/src/components/tracks/tracks1.js
+++ lyrics-sniffer/src/components/tracks/tracks1.js
@@ -1,4 +1,7 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../../App.css';
+
 
 export default function Tracks1(props) {
 
@@ -9,6 +12,14 @@ export default function Tracks1(props) {
                 <div className='card'>
                     <div className='card-content'>
                         <h5>{track.artist_name}</h5>
+                        <div class="content">
+                            <strong><i className='fas fa-play'></i> Track</strong>: {track.track_name}
+                            <br/>
+                            <strong><i className='fas fa-compact-disc'></i> Album</strong>: {track.album_name}
+                        </div>
+                        <Link to={`lyrics/track/${track.track_id}`}>
+                            <i className='fas fa-chevron-right'></i> View Lyrics
+                        </Link>
                     </div>
                 </div>
             </div>
